71e2378715a973a53e9f41d9adbc98265353f4fc
Alexander Kotliarov <slicer256@gmail.com>
2017-03-28 21:21:44 +0300

    working frames switching

--- app/app.js
+++ app/app.js
@@ -1,6 +1,32 @@
 new Vue({
 	el: '#app',
+	
 	data: {
-		test: 'working!'
+		frames: [],
+		total_images: 0,
+		images_loaded: 0,
+		current_frame: 0
+	},
+	
+	methods: {
+		frameLoaded: function(e) {
+			this.images_loaded++;
+		}
+	},
+	
+	created: function() {
+		this.$http.get('/api/list').then(responce => {
+			var i = 0;
+			for (var frame of responce.body) {
+				this.frames.push({
+					no: i,
+					url: '/upload/screenshots/' + frame.filename
+				});
+				i++;
+			}
+			this.total_images = this.frames.length;
+		}, console.error);
 	}
+	
 });
+
