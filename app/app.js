1ae005d49f75e261f2828ff9fcd60cd58cf470d6
Alexander Kotliarov <slicer256@gmail.com>
2017-03-29 19:21:16 +0300

    working play controls

--- app/app.js
+++ app/app.js
@@ -9,16 +9,42 @@ new Vue({
 		frames: [],
 		total_images: 0,
 		images_loaded: 0,
-		current_frame: 0
+		current_frame: 0,
+		is_playing: false,
+		play_speed: 200, // delay between frames switching
 	},
 	
 	methods: {
 		frameLoaded: function(e) {
 			this.images_loaded++;
+		},
+		
+		play: function(force) {
+			if (force) {
+				this.is_playing = true;
+			}
+			
+			this.next_frame(1);
+			
+			if (this.is_playing) {
+				setTimeout(this.play, this.play_speed);
+			}
+		},
+		
+		stop: function() {
+			this.is_playing = false;
+		},
+		
+		next_frame: function(delta) {
+			var new_val = parseInt(this.current_frame) + delta;
+			if (new_val > 0 && new_val < this.total_images) {
+				this.current_frame = new_val;
+			}
 		}
 	},
 	
 	created: function() {
+		//return true;
 		this.$http.get(config.api_host + '/api/list').then(responce => {
 			var i = 0;
 			for (var frame of responce.body) {
