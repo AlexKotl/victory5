4f9a7a03e443c38ca22db94e3982a4245840218e
Alexander Kotliarov <slicer256@gmail.com>
2017-04-01 21:01:36 +0300

    api fix for server bug

--- controllers/api.php
+++ controllers/api.php
@@ -25,26 +25,38 @@ class API {
     }
 
     public function legend() {
-
+	    
         $this->data = '';
 
         foreach ($this->ci->routes as $k => $v) {
             $this->data .= "<h2>{$k}</h2> {$v['description']}";
         }
+        
+        echo $this->data;
 
     }
     
     public function getScreenshots() {
-        if ($_REQUEST['period'] == 30) {
+        
+        $add_query = '';   
+        $time = '';
+        $period = '';      
+        if (isset($_GET['time'])) $time = $_GET['time'];
+        if (isset($_GET['period'])) $period = $_GET['period'];
+        
+        
+        if ($period == 30) {
 	        $add_query = " and timestamp > " . (time() - 60*60*24 * 30);
         }
-        
+
+
+
         $res = $this->db->query("SELECT * from screenshots where flag=1 {$add_query} order by id");
         while ($row = $res->fetch()) {
 	        
 	        $hour = (int)date('G', $row['timestamp']);
-	        if ($_REQUEST['time']=='day' && ($hour>17 || $hour<8)) continue;
-	        if ($_REQUEST['time']=='night' && ($hour>7 && $hour<19)) continue;
+	        if ($time == 'day' && ($hour>17 || $hour<8)) continue;
+	        if ($time == 'night' && ($hour>7 && $hour<19)) continue;
 
             if (file_exists($this->path . "/" . $row['filename'])) {
                 $this->data[] = $row;
@@ -52,9 +64,13 @@ class API {
 
         }
         
+        //print_r($this->data);
+        echo json_encode($this->data);
+        
     }
 
     public function __destruct() {
+		return true;
 
         if (is_array($this->data)) {
             header('Content-Type: application/json');
